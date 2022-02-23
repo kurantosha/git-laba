96565a87203d7ce4685153b8f1aa857d7a820b82
“kurantoshagit config --global user.name “kurantosha <kurantosha@gmail.com>
2022-02-23 20:15:21 +0200

    Edited name of method

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,10 +1,10 @@
 public class Task1 {
 
-    public static int triArea(int base, int height) {
+    public static int area(int base, int height) {
         return (base * height) / 2;
     }
 
     public static void main(String[] args) {
-        System.out.println(triArea(7,137));
+        System.out.println(area(7,137));
     }
 }
