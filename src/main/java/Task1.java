7b2cb6dc0f6525cf46501e086bb59e50e515ec8b
“kurantoshagit config --global user.name “kurantosha <kurantosha@gmail.com>
2022-02-23 19:58:13 +0200

    Edit file Task1.java: deleted sout

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -5,8 +5,6 @@ public class Task1 {
     }
 
     public static void main(String[] args) {
-        System.out.println(triArea(3, 2));
-        System.out.println(triArea(7, 4));
-        System.out.println(triArea(10, 10));
+        System.out.println(triArea(8, 7));
     }
 }
