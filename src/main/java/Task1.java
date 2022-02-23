0ae02a5315db02884e2b8a55cb5cc537c793ec84
“kurantoshagit config --global user.name “kurantosha <kurantosha@gmail.com>
2022-02-23 19:47:05 +0200

    Added file of my project

---
+++ src/main/java/Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+
+    public static int triArea(int base, int height) {
+        return (base * height) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(triArea(3, 2));
+        System.out.println(triArea(7, 4));
+        System.out.println(triArea(10, 10));
+    }
+}
